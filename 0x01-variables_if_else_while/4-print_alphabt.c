9513b85052461a6e38c2c8167d29ba74938cac08
Amos Amissah <39559758+theonlyamos@users.noreply.github.com>
2023-02-17 23:19:38 +0000

    commit 4.10

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -15,7 +15,7 @@ char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
 for (i = 0; i < 26; i++)
 {
 if ((alphabets[i] != 'e') && (alphabets[i] != 'q'))
-        putchar(alphabets[i]);
+    putchar(alphabets[i]);
 }
 putchar('\n');
 return (0);
