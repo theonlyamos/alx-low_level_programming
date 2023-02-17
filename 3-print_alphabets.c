6bb21d51bfbefbc858ea77cb320d5d98f0a32199
Amos Amissah <39559758+theonlyamos@users.noreply.github.com>
2023-02-17 22:55:54 +0000

    commit 3.1

---
+++ 3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program to printout the alphabelts
+ * in lower cases and then in upper cases
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	char lowercases[] = "abcdefghijklmnopqrstuvwxyz";
+	char uppercases[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	for (i = 0; i < 26; i++)
+		putchar(lowercases[i]);
+	for (i = 0; i < 26; i++)
+		putchar(uppercases[i]);
+	putchar('\n');
+	return (0);
+}
