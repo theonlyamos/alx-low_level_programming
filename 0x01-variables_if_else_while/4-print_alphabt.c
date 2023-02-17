2a68fb7690e14a2621a87803753e7a1dedaea3ee
Amos Amissah <39559758+theonlyamos@users.noreply.github.com>
2023-02-17 23:02:04 +0000

    commit 4.0

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program to printout the alphabelts
+ * in lower cases
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i < 26; i++)
+    {
+        if (alphabets[i] != 'e' && alphabets[i] != 'q')
+            putchar(alphabets[i]);
+    }
+	putchar('\n');
+	return (0);
+}
