96ea9a4576583a9cc3619a13c699d867fceda1c4
Amos Amissah <theonlyamos@gmail.com>
2023-02-17 13:42:37 -0800

    1.1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_digit = n%10;
+	last_digit = n % 10;
 	printf("Last digit of %d is %d and is ", n, last_digit);
 	if (last_digit == 0)
 		printf("0\n");
